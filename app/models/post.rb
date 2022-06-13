2f7876215b88e1ae3d02b4ceede62653bf6232bd
Khine <mknyein00@gmail.com>
2022-06-13 15:50:06 +0900

    Post model require fields validations and test cases for these fields are added.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+
   belongs_to :author
   has_many :elements
 
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,3 +5,32 @@ require 'minitest_helper'
 #   #   assert true
 #   # end
 # end
+
+describe Post do
+  before do 
+    @current_author = Author.first_or_create(email: 'test@mail.com', password: '12345678')
+    @post = @current_author.posts.new
+  end
+
+  describe 'when creating new post without title' do
+    it 'should fail' do
+      @post.title = ''
+      @post.description = "This is the descripiton"
+      _(@post.valid?).must_equal false
+    end
+  end
+  describe 'when creating new post without description' do
+    it 'should fail' do
+      @post.title = 'This is title'
+      @post.description = ""
+      _(@post.valid?).must_equal false
+    end
+  end
+  describe 'when creating new post with title and description' do
+    it 'should success' do
+      @post.title = 'This is title'
+      @post.description = "This is the descripiton"
+      _(@post.save).must_equal true
+    end
+  end
+end
