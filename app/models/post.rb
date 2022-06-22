2f305eb7cbe2273a531986740aa9633146551309
Khine <mknyein00@gmail.com>
2022-06-22 17:25:09 +0900

    initialized the published post in home controller show in the home/index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: 25..250
 
   belongs_to :author
   has_many :elements
 
   has_one_attached :header_image
+  
+  scope :published, -> do
+    where(published: true)
+  end
+
+  scope :most_recently_published, -> do
+    order(published_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     end
   end
   
-  root to: "home#index"
+  root to: "readers/home#index"
 end
