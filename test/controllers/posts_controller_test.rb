1358a87c93853614f86c97710525cc2287003adb
Khine <mknyein00@gmail.com>
2022-07-02 16:04:47 +0900

    test for post model - fixed

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,49 +1,49 @@
 require 'minitest_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @current_author = Author.first_or_create(email: 'test@mail.com', password: '12345678')
-  #   @post = @current_author.posts.new
-  # end
+  setup do
+    sign_in(authors(:one))
+    @post = @current_author.posts.new
+  end
 
-  # test "should get index" do
-  #   get posts_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get posts_url
+    assert_response :success
+  end
 
-  # test "should get new" do
-  #   get new_post_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get new_post_url
+    assert_response :success
+  end
 
-  # test "should create post" do
-  #   assert_difference('Post.count') do
-  #     post posts_url, params: { post: { author_id: @post.author_id, description: @post.description, published: @post.published, published_at: @post.published_at, title: @post.title } }
-  #   end
+  test "should create post" do
+    assert_difference('Post.count') do
+      post posts_url, params: { post: { author_id: @post.author_id, description: @post.description, published: @post.published, published_at: @post.published_at, title: @post.title } }
+    end
 
-  #   assert_redirected_to post_url(Post.last)
-  # end
+    assert_redirected_to post_url(Post.last)
+  end
 
-  # # test "should show post" do
-  # #   get post_url(@post)
-  # #   assert_response :success
-  # # end
+  test "should show post" do
+    get post_url(@post)
+    assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get edit_post_url(@post)
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get edit_post_url(@post)
+    assert_response :success
+  end
 
-  # test "should update post" do
-  #   patch post_url(@post), params: { post: { author_id: @post.author_id, description: @post.description, published: @post.published, published_at: @post.published_at, title: @post.title } }
-  #   assert_redirected_to post_url(@post)
-  # end
+  test "should update post" do
+    patch post_url(@post), params: { post: { author_id: @post.author_id, description: @post.description, published: @post.published, published_at: @post.published_at, title: @post.title } }
+    assert_redirected_to post_url(@post)
+  end
 
-  # test "should destroy post" do
-  #   assert_difference('Post.count', -1) do
-  #     delete post_url(@post)
-  #   end
+  test "should destroy post" do
+    assert_difference('Post.count', -1) do
+      delete post_url(@post)
+    end
 
-  #   assert_redirected_to posts_url
-  # end
+    assert_redirected_to posts_url
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,23 +2,36 @@ require 'minitest_helper'
 
 class PostTest < ActiveSupport::TestCase
   setup do 
-    @current_author = Author.first_or_create(email: 'test@mail.com', password: '12345678')
-    @post = @current_author.posts.new
+    @current_author = authors(:one)
   end
 
-  test "post should fail to creat without title" do
-    @post.title = ''
-    @post.description = "This is the descripiton"
+  test "should fail to creat without title" do
+    post_params = {title: '', description: 'This is the descripiton'}
+    @post = @current_author.posts.new(post_params)
     assert_not @post.save
   end
-  test "post should fail to create without description" do
-    @post.title = 'This is the title'
-    @post.description = ""
+
+  test "should fail to create without description" do
+    post_params = {title: 'This is the title', description: ''}
+    @post = @current_author.posts.new(post_params)
     assert_not @post.save
   end
-  test "post should pass to create with title and description" do
-    @post.title = 'This is the title'
-    @post.description = "This is the descripiton"
+
+  test "should fail to create with title and description less than 25 chars" do
+    post_params = {title: 'This is the title', description: 'This is the descripiton'}
+    @post = @current_author.posts.new(post_params)
+    assert_not @post.save
+  end
+
+  test "should pass to create with title and description" do
+    post_params = {title: 'This is the title', description: 'This is the long descripiton for the post.'}
+    @post = @current_author.posts.new(post_params)
     assert @post.save
+  end
+
+  test "should return published posts" do
+    @posts = Post.published
+    @first_post = @posts.first
+    assert_equal true, @first_post.published
   end
 end
