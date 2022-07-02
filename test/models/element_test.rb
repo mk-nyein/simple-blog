2fdbc41b0c91bcddf67cb788615879421583db96
Khine <mknyein00@gmail.com>
2022-07-02 16:25:08 +0900

    testcases for element model - updated

--- test/models/element_test.rb
+++ test/models/element_test.rb
@@ -2,22 +2,36 @@ require 'minitest_helper'
 
 class ElementTest < ActiveSupport::TestCase
   setup do
-    @current_author = Author.first_or_create(email: 'test@gmail.com', password: '12345678')
-    @post = @current_author.posts.first_or_create(title: 'Sample Title', description: 'This is the sample description')
-    @element = @post.elements.new
+    @current_author = authors(:one)
+    @post = posts(:one)
   end
-  test " should not create without post_id" do
+
+  test "should not create without post_id" do
     @element = Element.new
     assert_not @element.valid?
   end
-  test " should not create without element_type" do
-    @element = @post.elements.new
+
+  test "should not create without element_type" do
+    @element = @post.elements.new()
     assert_not @element.valid?
   end
-  test " should not create if element_type is empty" do
-    @element = @post.elements.new
-    @element.element_type = ""
+  
+  test "should not create if element_type is empty" do
+    element_params = {element_type: ""}
+    @element = @post.elements.new(element_params)
     assert_not @element.valid?
+  end
+
+  test "should create for paragraph element" do
+    element_params = {element_type: 'paragraph', content: "<div>It is an example paragraph </div>"}
+    @element = @post.elements.new(element_params)
+    assert @element.save
+  end
+
+  test "should create for image element" do
+    element_params = {element_type: 'image' }
+    @element = @post.elements.new(element_params)
+    assert @element.save
   end
 
 end
