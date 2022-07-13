1051ae10f908f43f7b4ee9f17d2b34565bc30a6c
Khine <mknyein00@gmail.com>
2022-07-13 15:20:13 +0900

    Add test for most_recent_post scope in post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,6 +3,7 @@ require 'minitest_helper'
 class PostTest < ActiveSupport::TestCase
   setup do 
     @current_author = authors(:one)
+    @post_one = posts(:one)
   end
 
   test "should fail to creat without title" do
@@ -33,5 +34,13 @@ class PostTest < ActiveSupport::TestCase
     @posts = Post.published
     @first_post = @posts.first
     assert_equal true, @first_post.published
+  end
+
+  test "should return published posts in decending order of published date" do
+    @post_one.update(published: true, published_at: DateTime.now)
+    @posts = Post.most_recently_published
+    assert_equal @post_one.id, @posts.first.id
+    assert_equal @post_one.published_at, @posts.first.published_at
+    assert_equal true, @posts.first.published
   end
 end
